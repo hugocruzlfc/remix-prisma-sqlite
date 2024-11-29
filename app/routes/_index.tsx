8353702cc45bfcf988673aa2078ed465bd0959eb
Hugo Cruz <hugocruzlfc@gmail.com>
2024-11-29 13:47:25 +0100

    feat: add ui index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, ActionFunction } from "@remix-run/node";
 import { prisma } from "@/lib/prisma-client";
 import { Button } from "@/components/ui/button";
+import { Form, Link, useLoaderData, useNavigation } from "@remix-run/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,16 +18,76 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const values = Object.fromEntries(formData);
+
+  await prisma.user.create({
+    data: {
+      username: values.username as string,
+      email: values.email as string,
+    },
+  });
+
+  await prisma.$disconnect();
+  return true;
+};
+
 export async function loader() {
   const allUsers = await prisma.user.findMany();
   await prisma.$disconnect();
+
   return allUsers;
 }
 
 export default function Index() {
+  const users = useLoaderData<typeof loader>();
+  const { state } = useNavigation();
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="p-5 flex flex-col items-center  space-y-5">
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Users and Bookmarks: Sample App with Remix and Prisma
+      </h2>
+      <Form
+        method="post"
+        className="w-96 p-5 space-y-2"
+      >
+        <Input
+          type="text"
+          name="username"
+          placeholder="User name"
+        />
+
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <Button
+          type="submit"
+          disabled={state === "submitting"}
+        >
+          {state === "submitting" ? "Adding User..." : "Add User"}
+        </Button>
+      </Form>
+      {users.map((user) => (
+        <Card
+          key={user.id}
+          className="w-96 flex flex-row items-center justify-between"
+        >
+          <CardHeader>
+            <CardTitle>{user.username}</CardTitle>
+            <CardDescription>{user.email}</CardDescription>
+          </CardHeader>
+
+          <CardContent className="text-right">
+            <Link to={`/bookmarks/${user.id}`}>
+              <Button variant="link">View Details</Button>
+            </Link>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
